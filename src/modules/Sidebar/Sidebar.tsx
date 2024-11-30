bb03a30ee27d0634a699f0d6900d04dc566219c8
Hashirooy <battlenetruss@gmail.com>
2024-11-30 18:33:54 +0700

    good

--- src/modules/Sidebar/Sidebar.tsx
+++ src/modules/Sidebar/Sidebar.tsx
@@ -1,14 +1,42 @@
-import "./Sidebar.css"
+import "./Sidebar.css";
 
-export const SideBar = ()=>{
-    return( <div className="sidebar">
-        <div className="sibebar-options">
-        <a href="">Главнвя</a>
-        <a href="">Фильмы</a>
-        <a href="">Сериалы</a>
-        <a href="">Спорт</a>
-        <a href="">Игры</a>
-        <a href="">Медиа</a>
+export const SideBar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sibebar-options">
+        <div className="sibebar-options_element">
+          <img src="src\img\home.svg" alt="" />
+          <a href="">Главная</a>
         </div>
-      </div>)
-}
+        <div className="sibebar-options_element">
+          <img src="src\img\film.svg" alt="" />
+          <a href="">Фильмы</a>
+        </div>
+        <div className="sibebar-options_element">
+          <img src="src\img\play.svg" alt="" />
+          <a href="">Сериалы</a>
+        </div>
+        <div className="sibebar-options_element">
+          <img src="src\img\ball.svg" alt="" />
+          <a href="">Спорт</a>
+        </div>
+        <div className="sibebar-options_element">
+          <img src="src\img\tv.svg" alt="" />
+          <a href="">Игры</a>
+        </div>
+        <div className="sibebar-options_element">
+          <img src="src\img\media.svg" alt="" />
+          <a href="">Медиа</a>
+        </div>
+        <div className="sibebar-options_element">
+          <img src="src\img\serial.svg" alt="" />
+          <a href="">Телеканалы</a>
+        </div>
+        <div className="sibebar-options_element">
+          <img src="src\img\media.svg" alt="" />
+          <a href="">Онлайн-кинотеатр</a>
+        </div>
+      </div>
+    </div>
+  );
+};
